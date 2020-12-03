f68f9245485b9b2fe8e2b6ae6ec3045e5c58d8a2
Eli Bishop <eli@launchdarkly.com>
2020-12-02 19:40:06 -0800

    use new jsonstream API

--- ldmodel/model_serialization.go
+++ ldmodel/model_serialization.go
@@ -1,17 +1,49 @@
 package ldmodel
 
+import (
+	"gopkg.in/launchdarkly/go-jsonstream.v1/jreader"
+	"gopkg.in/launchdarkly/go-jsonstream.v1/jwriter"
+)
+
 // DataModelSerialization defines an encoding for SDK data model objects.
 //
 // For the default JSON encoding used by LaunchDarkly SDKs, use NewJSONDataModelSerialization.
 type DataModelSerialization interface {
 	// MarshalFeatureFlag converts a FeatureFlag into its serialized encoding.
 	MarshalFeatureFlag(item FeatureFlag) ([]byte, error)
+
 	// MarshalSegment converts a Segment into its serialized encoding.
 	MarshalSegment(item Segment) ([]byte, error)
+
 	// UnmarshalFeatureFlag attempts to convert a FeatureFlag from its serialized encoding.
 	UnmarshalFeatureFlag(data []byte) (FeatureFlag, error)
+
 	// UnmarshalFeatureFlag attempts to convert a FeatureFlag from its serialized encoding.
 	UnmarshalSegment(data []byte) (Segment, error)
+}
+
+// MarshalFeatureFlagToJSONWriter attempts to convert a FeatureFlag to JSON using the jsonstream API.
+// For details, see: https://github.com/launchdarkly/go-jsonstream
+func MarshalFeatureFlagToJSONWriter(item FeatureFlag, writer *jwriter.Writer) {
+	marshalFeatureFlagToWriter(item, writer)
+}
+
+// MarshalSegmentToJSONWriter attempts to convert a Segment to JSON using the jsonstream API.
+// For details, see: https://github.com/launchdarkly/go-jsonstream
+func MarshalSegmentToJSONWriter(item Segment, writer *jwriter.Writer) {
+	marshalSegmentToWriter(item, writer)
+}
+
+// UnmarshalFeatureFlagFromJSONReader attempts to convert a FeatureFlag from JSON using the jsonstream
+// API. For details, see: https://github.com/launchdarkly/go-jsonstream
+func UnmarshalFeatureFlagFromJSONReader(reader *jreader.Reader) FeatureFlag {
+	return unmarshalFeatureFlagFromReader(reader)
+}
+
+// UnmarshalSegmentFromJSONReader attempts to convert a Segment from JSON using the jsonstream API.
+// For details, see: https://github.com/launchdarkly/go-jsonstream
+func UnmarshalSegmentFromJSONReader(reader *jreader.Reader) Segment {
+	return unmarshalSegmentFromReader(reader)
 }
 
 type jsonDataModelSerialization struct{}
@@ -35,11 +67,11 @@ func (s jsonDataModelSerialization) MarshalSegment(item Segment) ([]byte, error)
 }
 
 func (s jsonDataModelSerialization) UnmarshalFeatureFlag(data []byte) (FeatureFlag, error) {
-	return unmarshalFeatureFlag(data)
+	return unmarshalFeatureFlagFromBytes(data)
 }
 
 func (s jsonDataModelSerialization) UnmarshalSegment(data []byte) (Segment, error) {
-	return unmarshalSegment(data)
+	return unmarshalSegmentFromBytes(data)
 }
 
 // MarshalJSON overrides the default json.Marshal behavior to provide the same marshalling behavior that is
@@ -57,7 +89,7 @@ func (s Segment) MarshalJSON() ([]byte, error) {
 // UnmarshalJSON overrides the default json.Unmarshal behavior to provide the same unmarshalling behavior that
 // is used by NewJSONDataModelSerialization().
 func (f *FeatureFlag) UnmarshalJSON(data []byte) error {
-	result, err := unmarshalFeatureFlag(data)
+	result, err := unmarshalFeatureFlagFromBytes(data)
 	if err == nil {
 		*f = result
 	}
@@ -67,7 +99,7 @@ func (f *FeatureFlag) UnmarshalJSON(data []byte) error {
 // UnmarshalJSON overrides the default json.Unmarshal behavior to provide the same unmarshalling behavior that
 // is used by NewJSONDataModelSerialization().
 func (s *Segment) UnmarshalJSON(data []byte) error {
-	result, err := unmarshalSegment(data)
+	result, err := unmarshalSegmentFromBytes(data)
 	if err == nil {
 		*s = result
 	}
