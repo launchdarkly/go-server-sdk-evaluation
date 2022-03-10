a90569e20a2adf9e527f6e517858443abc09d217
Eli Bishop <eli@launchdarkly.com>
2022-03-09 21:23:37 -0800

    rm debugging

--- evaluator_benchmarks_test.go
+++ evaluator_benchmarks_test.go
@@ -14,25 +14,6 @@ import (
 	"gopkg.in/launchdarkly/go-sdk-common.v3/ldvalue"
 )
 
-func BenchmarkEvaluationTempDebuggingTest(b *testing.B) {
-	cases := []evalBenchmarkCase{
-		{
-			numRules:          1,
-			numClauses:        1,
-			operator:          ldmodel.OperatorIn,
-			shouldMatchClause: true,
-			prereqsWidth:      5,
-			prereqsDepth:      1,
-		},
-	}
-	benchmarkEval(b, cases, func(env *evalBenchmarkEnv) {
-		evalBenchmarkResult = env.evaluator.Evaluate(env.targetFlag, env.user, discardPrerequisiteEvents)
-		if !evalBenchmarkResult.Value.BoolValue() { // verify that we got a match
-			b.FailNow()
-		}
-	})
-}
-
 var evalBenchmarkResult ldreason.EvaluationDetail
 var evalBenchmarkErr error
 
