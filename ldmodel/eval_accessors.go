ed5c9351dcfb137a8897fb769ca28f49ca1c979b
Eli Bishop <eli@launchdarkly.com>
2022-06-16 19:31:05 -0700

    implement aliasing of "anonymous" to "transient" in evaluations

--- ldmodel/eval_accessors.go
+++ ldmodel/eval_accessors.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"time"
 
+	"github.com/launchdarkly/go-sdk-common/v3/ldcontext"
 	"github.com/launchdarkly/go-sdk-common/v3/ldvalue"
 	"github.com/launchdarkly/go-semver"
 )
@@ -134,6 +135,22 @@ func (e EvaluatorAccessorMethods) ClauseGetValueAsTimestamp(clause *Clause, inde
 		return TypeConversions.ValueToTimestamp(clause.Values[index])
 	}
 	return time.Time{}, false
+}
+
+// ClauseGetContextAttributeValue retrieves an attribute from a Context corresponding to the clause's
+// Attribute-- unless we have determined that the attribute is really an alias for another attribute.
+func (e EvaluatorAccessorMethods) ClauseGetContextAttributeValue(
+	clause *Clause,
+	context *ldcontext.Context,
+) ldvalue.Value {
+	if clause == nil || context == nil {
+		return ldvalue.Null()
+	}
+	if clause.preprocessed.realAttr.IsDefined() {
+		return context.GetValueForRef(clause.preprocessed.realAttr)
+		// This allows us to skip the extra comparison step below
+	}
+	return context.GetValueForRef(resolveAttributeAliases(clause.Attribute))
 }
 
 // SegmentFindKeyInExcluded returns true if the specified key is in this Segment's
