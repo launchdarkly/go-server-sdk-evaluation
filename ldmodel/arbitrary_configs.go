db4fdbacc5008c3ac33259ed9526d15c2d86c809
Shalini Singh <88707003+singhshalinis01@users.noreply.github.com>
2025-02-12 12:38:29 -0800

    Add a new config

---
+++ ldmodel/arbitrary_configs.go
@@ -0,0 +1,19 @@
+package ldmodel
+
+// ArbitraryConfigs describes an individual arbitrary configuration.
+//
+// The fields of this struct are exported for use by LaunchDarkly internal components.
+type ArbitraryConfigs struct {
+	// Key is the unique string key of the arbitrary configuration.
+	Key string
+
+	// DataType represents the data type of the configuration.
+	DataType string
+
+	// Values contains any associated configuration values.
+	Values any
+
+	// Version is an integer that is incremented by LaunchDarkly every time the configuration is
+	// changed.
+	Version int
+} 
